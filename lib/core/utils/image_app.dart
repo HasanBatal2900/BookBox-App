b15e694ed6ee68e3d33d893b38ed4b1dc874c346
Hasan Batal <hh0657768@gmail.com>
2025-03-06 12:33:00 -0800

    adding image_app file

--- lib/core/utils/image_app.dart
+++ lib/core/utils/image_app.dart
@@ -1,5 +1,6 @@
-class ImageApp{
-
+class ImageApp {
   ImageApp._();
   static const String logo = 'assets/images/logo.png';
+  static const String wavyLines = "assets/images/lines.png";
+  static const String onBoarding = "assets/images/person.png";
 }
