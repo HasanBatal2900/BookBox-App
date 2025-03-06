1c1e193a7f03cc1dfcd862b4b47d190f84663c5d
Hasan Batal <hh0657768@gmail.com>
2025-03-06 12:51:21 -0800

    adding bottom navigation bar to the app

--- lib/core/widgets/tabs/scaffold_with_bottom_nav_bar.dart
+++ lib/core/widgets/tabs/scaffold_with_bottom_nav_bar.dart
@@ -1,20 +1,20 @@
-// import 'package:ecommerce_app/core/widgets/tabs/bottom_navigation_bottom.dart';
-// import 'package:flutter/material.dart';
+import 'package:book_box/core/widgets/tabs/bottom_navigation_bottom.dart';
+import 'package:flutter/material.dart';
 
-// class ScaffoldWithNavBar extends StatelessWidget {
-//   const ScaffoldWithNavBar({
-//     required this.currentScreen,
-//     Key? key,
-//   }) : super(key: key);
+class ScaffoldWithNavBar extends StatelessWidget {
+  const ScaffoldWithNavBar({
+    required this.currentScreen,
+    Key? key,
+  }) : super(key: key);
 
-//   final Widget currentScreen;
+  final Widget currentScreen;
 
-//   @override
+  @override
 
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: currentScreen,
-//       bottomNavigationBar: const HomeBottomNavigationBar(),
-//     );
-//   }
-// }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: currentScreen,
+      bottomNavigationBar: const HomeBottomNavigationBar(),
+    );
+  }
+}
