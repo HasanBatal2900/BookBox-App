b12aef7b8281e5d7e014d429fd15072075cb6055
Hasan Batal <hh0657768@gmail.com>
2025-03-08 13:23:50 -0800

    adding the route of  book details view

--- lib/router/router_list.dart
+++ lib/router/router_list.dart
@@ -1,5 +1,6 @@
 import 'package:book_box/core/helper/navigaton/nav2.0.dart';
 import 'package:book_box/core/widgets/tabs/scaffold_with_bottom_nav_bar.dart';
+import 'package:book_box/features/book/presentation/views/book_details_view.dart';
 import 'package:book_box/features/book/presentation/views/favoriate_book_view.dart';
 import 'package:book_box/features/book/presentation/views/home_book_view.dart';
 import 'package:book_box/features/onboarding/presentation/views/onboarding_view.dart';
@@ -31,6 +32,13 @@ List<RouteBase> routerList = [
         pageBuilder: (context, state) =>
             ScaleNavigation(screen: const FavoriateBookView()),
       ),
+      GoRoute(
+        path: BookDetailsView.route,
+        pageBuilder: (context, state) => SlideToRightNavigation(
+          screen: const BookDetailsView(),
+          // add param here to pass it to the view
+        ),
+      )
     ],
   ),
 ];
