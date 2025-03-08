53ea2d89c9a008dda1647bf69a227b1523f8a296
Hasan Batal <hh0657768@gmail.com>
2025-03-08 12:57:29 -0800

    add images to the app

--- lib/core/utils/image_app.dart
+++ lib/core/utils/image_app.dart
@@ -3,4 +3,5 @@ class ImageApp {
   static const String logo = 'assets/images/logo.png';
   static const String wavyLines = "assets/images/lines.png";
   static const String onBoarding = "assets/images/person.png";
+  static const String bookImage = "assets/images/book_image.png";
 }
